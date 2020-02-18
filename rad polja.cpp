174a171f9efc4dd41c3e2d6ab73a51e141a581c9
lukajovicic <58472126+lukajovicic@users.noreply.github.com>
2020-02-18 14:01:45 +0100

    Add files via upload

---
+++ rad polja.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout<<"unesi broj"<<endl;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+		cout<<"upisi "<<i+1<<" vrijednost u polju:"<<endl;
+		cin>>a[i];
+	}
+	int b=a[n];
+	for(int i=0;i<n;i++){
+		
+	}
+	
+	system("pause");
+	return 0;
+}
